112e3a10e1c6197f54602604631baa546c015c73
Caleb Bush <calebmatthewbush@gmail.com>
2024-01-04 17:16:32 -0800

    Built out utility and state management functions

--- script.js
+++ script.js
@@ -3,55 +3,94 @@ const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm
 
 let currentWordCoord = 3
 let currentLetterCoord = 0
+let requiredLetters = ['a', 'n']
+let currentLetters = []
 
+//DOM manipulation
 
-const btn = document.getElementById('btn')
-btn.addEventListener('click', () => checkWord())
-
-const captureContent = () => {
-    const text = document.getElementById('word').value
-    return text
-}
-
-const placeLetter = (wordCoord, letterCoord, letterToPlace) => {
+const placeLetterInDOM = (wordCoord, letterCoord, letterToPlace) => {
     const box = document.getElementById(`word-${wordCoord}-letter-${letterCoord}`)
     box.textContent = letterToPlace
 }
 
-const removeLetter = (wordCoord, letterCoord) => {
+const removeLetterFromDOM = (wordCoord, letterCoord) => {
     const box = document.getElementById(`word-${wordCoord}-letter-${letterCoord}`)
     box.textContent = ''
 }
+
+//State management
 
 const setCoords = (wordCoord, letterCoord) => {   
     currentWordCoord = wordCoord
     currentLetterCoord = letterCoord
 }
 
-async function checkWord(){
-    const wordToCheck = captureContent()
-    const result = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToCheck}`)
-    console.log(result)
+const updateCurrentLetters = (array, letter) => {
+    if (letter){
+        if(array.includes(letter)){
+            return
+        }
+        array.push(letter)
+        return
+    }
+    array.pop()
+}
+
+// const updateRequiredLetters = (required, wordArray) => {
+//     wordArray.forEach((letter) => {
+//         if (!required.includes(letter)){
+
+//         }
+//     })
+// }
+
+//Utility Functions
+
+const verifyRequiredLetterUsage = (requiredLetters, lettersToCheck) => {
+    //Update this function. ForEach is not appropriate here.
+    //Make sure each unique letter is in the submitted word.
+    requiredLetters.forEach((letter) => {
+        if (!lettersToCheck.includes(letter)){
+            return
+        }
+    })
+    return true
+} 
+
+async function checkDictionaryForWord(word){
+    const result = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     return result
 }
 
 const validKeyCheck = (pressedKey) => {
-    console.log(pressedKey.key)
-    console.log('word: ', currentWordCoord)
-    console.log('letter: ', currentLetterCoord)
-
     const uppercaseKey = pressedKey.key.toUpperCase()
     
     if (alphabet.includes(pressedKey.key.toLowerCase())){
         if (currentLetterCoord < currentWordCoord){
-            placeLetter(currentWordCoord, currentLetterCoord, uppercaseKey)
+            updateCurrentLetters(currentLetters, pressedKey.key.toLowerCase())
+            placeLetterInDOM(currentWordCoord, currentLetterCoord, uppercaseKey)
             setCoords(currentWordCoord, currentLetterCoord + 1)
+
         }
     } else if (pressedKey.key == 'Enter' && currentLetterCoord >= currentWordCoord){
-        console.log('full')
-        setCoords(currentWordCoord + 1, 0)
+        const wordToCheck = currentLetters.join('')
+        //Need to verify if required letters are used and it's a valid word
+        if(verifyRequiredLetterUsage(requiredLetters, currentLetters)){
+            checkDictionaryForWord(wordToCheck).then((response) => {
+                const status = response.status
+                if(status == 200){
+                    
+                    currentLetters = []
+                    setCoords(currentWordCoord + 1, 0)
+                }
+                else if(status == 404){
+                    console.log('invalid')
+                } else console.log(status)
+            })
+        }
     } else if (pressedKey.key == 'Backspace' && currentLetterCoord !== 0){
-        removeLetter(currentWordCoord, currentLetterCoord - 1)
+        updateCurrentLetters(currentLetters)
+        removeLetterFromDOM(currentWordCoord, currentLetterCoord - 1)
         setCoords(currentWordCoord, currentLetterCoord - 1)
     }
 }
